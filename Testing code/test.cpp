2975a73821696cb23bd09da87e75daf66d792ec1
Henry Tseng <henrybear327@gmail.com>
2017-03-21 14:01:04 +0800

    Update test code

--- Testing code/test.cpp
+++ Testing code/test.cpp
@@ -1,13 +1,30 @@
 #include<bits/stdc++.h>
 
+int theSize(char a[])
+{
+	return sizeof(a);
+}
+
 int theSize(int b[])
 {
 	return sizeof(b);
 }
 
+
+int theSize(double c[])
+{
+	return sizeof(c);
+}
+
 int main()
 {
-	int a[10];
-	printf("%d %d\n", sizeof(a), theSize(a));
+	char a[10];
+	int b[10];
+	double c[10];
+	
+	//  g++ -Wall -Wextra -std=c++11 -O2 test.cpp -m32 && ./a.out
+	//  pointer size will be 32-bit
+	printf("%lu %lu %lu\n", sizeof(a), sizeof(b), sizeof(c));
+	printf("%d %d %d\n", theSize(a), theSize(b), theSize(c));
 	return 0;
 }
