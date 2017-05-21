616b6fa57beec5daaaf4d698de322bd3ad33874c
Henry Tseng <henrybear327@gmail.com>
2017-05-22 00:59:32 +0800

    Bug fix on string compare

--- Ass4/Hw4_403410033.cpp
+++ Ass4/Hw4_403410033.cpp
@@ -1,9 +1,9 @@
 /*
-Chun-Hung Tseng
-403410033
-Computer Science and Information Technology
-Junior year student
-*/
+   Chun-Hung Tseng
+   403410033
+   Computer Science and Information Technology
+   Junior year student
+ */
 
 #include <algorithm>
 #include <cstring>
@@ -11,11 +11,11 @@ Junior year student
 #include <string>
 
 /*
-0 normal mode
-1 default testing
-2 debug print
-*/
-#define DEBUG 1
+   0 normal mode
+   1 default testing
+   2 debug print
+ */
+#define DEBUG 0
 
 using namespace std;
 
@@ -36,12 +36,14 @@ public:
     BigInt(const BigInt &other);
     ~BigInt();
 
+    string toString() const;
+    string toCmpString(int n) const;
+
 private:
     BigInt(int *num, int dataSize, bool isNegative);
     int *data;
     int dataSize;
     bool isNegative;
-    string toString() const;
     BigInt negate() const;
 };
 
@@ -70,6 +72,16 @@ BigInt::BigInt(int num)
         num *= -1;
     } else
         isNegative = false;
+
+    if (num == 0) {
+        // initialize to 0
+        isNegative = false;
+        dataSize = 1;
+        data = new int[dataSize];
+
+        data[0] = 0;
+        return;
+    }
 
     int tmp = num;
     dataSize = 0;
@@ -130,6 +142,20 @@ string BigInt::toString() const
     return res;
 }
 
+string BigInt::toCmpString(int n) const
+{
+    // fill with leading 0
+    // 2 cmp with 10 != 2 cmp with 10
+    string res = "";
+
+    for (int i = 0; i < n - dataSize; i++)
+        res += "0";
+
+    for (int i = dataSize - 1; i >= 0; i--)
+        res += data[i] + '0';
+    return res;
+}
+
 ostream &operator<<(ostream &out, const BigInt &other)
 {
     cout << other.toString();
@@ -139,7 +165,7 @@ ostream &operator<<(ostream &out, const BigInt &other)
 
 BigInt BigInt::negate() const
 {
-    BigInt ret(*this);
+    BigInt ret(this->toString());
 
     if (ret.isNegative)
         ret.isNegative = false;
@@ -171,26 +197,28 @@ const BigInt BigInt::operator+(const BigInt &other) const
         return res;
     } else {
         if (isNegative == true) {
-            BigInt tmp = (*this);
-            return other - tmp.negate();
+            return other - (*this).negate();
         } else {
-            BigInt tmp = other;
-            return *this - tmp.negate();
+            return (*this) - other.negate();
         }
     }
 }
 
 const BigInt BigInt::operator-(const BigInt &other) const
 {
+    /*
+    cout << *this << " " << other << endl;
+    cout << (*this).toString() << " " << other.toString() << endl;
+    cout << ((*this).toString() < other.toString()) << endl;
+    cout << isNegative << " " << other.isNegative << endl;
+    */
+
     int mx = max(this->dataSize, other.dataSize);
 
     if (isNegative == false && other.isNegative == false) {
-        if ((*this).toString() < other.toString()) {
-            BigInt tmp = *this;
-            BigInt tmp1 = other;
-            BigInt res = (tmp1 - tmp);
-
-            return res.negate();
+        if ((*this).toCmpString(other.dataSize) <
+            other.toCmpString((*this).dataSize)) {
+            return (other - (*this)).negate();
         }
 
         int *res = new int[mx];
@@ -269,6 +297,123 @@ BigInt::~BigInt()
 #endif
 
     delete[] data;
+}
+
+string BigIntOutput(int i, int j)
+{
+    string res = "";
+    BigInt num1(i);
+    BigInt num2(j);
+
+    // BigInt num1(to_string(i));
+    // BigInt num2(to_string(j));
+
+    BigInt sum = num1 + num2;
+    res =
+        num1.toString() + " + " + num2.toString() + " = " + sum.toString() + "\n";
+
+    return res;
+}
+
+string normalOutout(int i, int j)
+{
+    string res = "";
+
+    res = to_string(i) + " + " + to_string(j) + " = " + to_string(i + j) + "\n";
+
+    return res;
+}
+
+void invoke(int type, int range)
+{
+    switch (type) {
+    case 1:
+        // type 1: + +
+        for (int i = 0; i < range; i++) {
+            if (i % 1000 == 0)
+                cout << "type 1, i = " << i << endl;
+
+            for (int j = 0; j < range; j++) {
+                string r1 = BigIntOutput(i, j);
+                string r2 = normalOutout(i, j);
+
+                if (r1 != r2) {
+                    cout << "Mismatch!!!" << endl;
+                    cout << r1;
+                    cout << r2;
+                    cout << endl;
+                }
+            }
+        }
+        break;
+    case 2:
+        // type 2: + -
+        for (int i = 0; i < range; i++) {
+            if (i % 1000 == 0)
+                cout << "type 2, i = " << i << endl;
+
+            for (int j = 0; j < range; j++) {
+                string r1 = BigIntOutput(i, -j);
+                string r2 = normalOutout(i, -j);
+
+                if (r1 != r2) {
+                    cout << "Mismatch!!!" << endl;
+                    cout << r1;
+                    cout << r2;
+                    cout << endl;
+                }
+            }
+        }
+        break;
+    case 3:
+        // type 3: - +
+        for (int i = 0; i < range; i++) {
+            if (i % 1000 == 0)
+                cout << "type 3, i = " << i << endl;
+
+            for (int j = 0; j < range; j++) {
+                string r1 = BigIntOutput(-i, j);
+                string r2 = normalOutout(-i, j);
+
+                if (r1 != r2) {
+                    cout << "Mismatch!!!" << endl;
+                    cout << r1;
+                    cout << r2;
+                    cout << endl;
+                }
+            }
+        }
+        break;
+    case 4:
+        // type 4: - -
+        for (int i = 0; i < range; i++) {
+            if (i % 1000 == 0)
+                cout << "type 4, i = " << i << endl;
+
+            for (int j = 0; j < range; j++) {
+                string r1 = BigIntOutput(-i, -j);
+                string r2 = normalOutout(-i, -j);
+
+                if (r1 != r2) {
+                    cout << "Mismatch!!!" << endl;
+                    cout << r1;
+                    cout << r2;
+                    cout << endl;
+                }
+            }
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+void tester(int range)
+{
+    for (int i = 1; i <= 4; i++) {
+        printf("Type %d testing...\n", i);
+        invoke(i, range);
+    }
 }
 
 int main()
@@ -351,6 +496,11 @@ int main()
     c = a - *b;
     cout << a << " - " << *b << " = " << c << endl;
 #endif
+
+    tester(5000);
+
+    // BigInt a(72), b(-913);
+    // cout << a + b << endl;
 
     return 0;
 }
